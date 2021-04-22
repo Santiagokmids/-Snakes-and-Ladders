e5622e19544a72c211e331881c61e5917c5dbea2
Luis Miguel <luismiguelossaarias05@gmail.com>
2021-04-22 09:04:27 -0500

    The methos who add a matrix has been added

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,9 +1,14 @@
 package ui;
 
+import model.SnakesAndLadders;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		SnakesAndLadders lm = new SnakesAndLadders(3, 4);
+		System.out.println(lm);
+		
 	}
 
 }
