933f2b29db86db1f8b16be2e840816154486d861
Santiago <sntgtrochez@gmail.com>
2021-04-24 21:39:51 -0500

    Almost ready the players with random symbol

--- src/model/SnakesAndLadders.java
+++ src/model/SnakesAndLadders.java
@@ -3,48 +3,48 @@ package model;
 import java.util.Random;
 
 public class SnakesAndLadders {
-	
+
 	private Node root;
 	private BestPlayers firstPlayer;
-	
+
 	private int matrixRows;
 	private int matrixCols;
-	
+
 	public SnakesAndLadders(int matrixRows, int matrixCols) {
 		this.matrixRows = matrixRows;
 		this.matrixCols = matrixCols;
 		createNewMatrix();
 	}
-	
+
 	private void createNewMatrix() {
 		root = new Node(0,0);
 		createNewRow(0,0,root);
 		asingPosition(0,matrixRows-1,0);
 	}
-	
+
 	private void createNewRow(int i, int j, Node currentRootRow) {
-		
+
 		createNewCol(i,j+1,currentRootRow,currentRootRow.getUp());
-		
+
 		if(i+1 < matrixRows) {
-			
+
 			Node downRootRow = new Node(i+1,j);
 			downRootRow.setUp(currentRootRow);
 			currentRootRow.setDown(downRootRow);
 			createNewRow(i+1,j,downRootRow);
 		}
 	}
-	
+
 	private void createNewCol(int i, int j, Node previous, Node rowPrevious) {
-		
+
 		if(j < matrixCols) {
-			
+
 			Node current = new Node(i,j);
 			current.setPrevious(previous);
 			previous.setNext(current);
-			
+
 			if(rowPrevious != null) {
-				
+
 				rowPrevious = rowPrevious.getNext();
 				current.setUp(rowPrevious);
 				rowPrevious.setDown(current);
@@ -52,52 +52,52 @@ public class SnakesAndLadders {
 			createNewCol(i, 1+j, current, rowPrevious);
 		}
 	}
-	
+
 	public void addPlayer(String name, long score, String symbol) {
-		
+
 		BestPlayers newPlayer = new BestPlayers(name,score,symbol);
-		
+
 		if(firstPlayer == null) {
 			firstPlayer = newPlayer;
 		}else {
 			addPlayer(firstPlayer,newPlayer);
 		}
 	}
-	
+
 	private void asingPosition(int position, int i,int j) {
-		
+
 		Node newNode = searchNode(i,j);
-		
+
 		if(newNode != null) {
 			asingNext(newNode,position+1);
 		}
 	}
-	
+
 	private void asingNext(Node newNode, int position) {
-		
+
 		newNode.setPosition(position);
-		
+
 		if(newNode.getNext() != null) {
 			asingNext(newNode.getNext(),position+1);
 		}else if(newNode.getUp() != null){
 			asingPrev(newNode.getUp(),position+1);
 		}
 	}
-	
+
 	private void asingPrev(Node newNode, int position) {
-		
+
 		newNode.setPosition(position);
-		
+
 		if(newNode.getPrevious() != null) {
-			
+
 			asingPrev(newNode.getPrevious(),position+1);
 		}else  if(newNode.getUp() != null){
 			asingNext(newNode.getUp(),position+1);
 		}
 	}
-	
+
 	private void addPlayer(BestPlayers current, BestPlayers newPlayer) {
-		
+
 		if(newPlayer.getScore() <= current.getScore()){
 			if(current.getNext() == null) {
 				current.setNext(newPlayer);
@@ -136,7 +136,7 @@ public class SnakesAndLadders {
 	public void setMatrixCols(int matrixCols) {
 		this.matrixCols = matrixCols;
 	}
-	
+
 	public String toString() {
 		String msg;
 		msg = toStringRow(root);
@@ -160,49 +160,49 @@ public class SnakesAndLadders {
 		}
 		return msg;
 	}
-	
+
 	public String toStringScoreTable() {
 		String message;
-		
+
 		if(firstPlayer != null) {
 			message = toStringScores(firstPlayer);
 		}else {
 			message = "---No hay ningun puntaje todavia---";
 		}
-	
+
 		return message;
 	}
-	
+
 	private String toStringScores(BestPlayers player) {
 		String message = "";
-		
+
 		if(player != null) {
 			message = player.toString();
-			
+
 			if(player.getNext() != null) {
 				message += "\n" + toStringScores(player.getNext());
 			}
 		}
-		
+
 		return message;
 	}
-	
+
 	public String toString2() {
 		String msg = "";
-		
+
 		return msg;
 	}
-	
+
 	public void addSettingSnake(int snakes) {
-		
+
 		Random azarRow = new Random();
 		Random azarCol = new Random();
-		
+
 		int selectedRow = (int)(azarRow.nextDouble() * getMatrixRows());
 		int selectedCol = (int)(azarCol.nextDouble() * getMatrixCols());
-		
+
 		Node searched = searchNode(selectedRow, selectedCol);
-		
+
 		if(searched.getPosition() != 1 && searched.getPosition() != (matrixCols*matrixRows) && snakes != 0 && searched.getSnake() == ' ' && searched.getLadder() == 0){
 			char letter = (char)('A'+searched.getCol());
 			searched.setSnake(letter);
@@ -213,17 +213,17 @@ public class SnakesAndLadders {
 			addSettingSnake(snakes);
 		}
 	}
-	
+
 	public void addSettingLadders(int ladders) {
-		
+
 		Random azarRow = new Random();
 		Random azarCol = new Random();
-		
+
 		int selectedRow = (int)(azarRow.nextDouble() * matrixRows);
 		int selectedCol = (int)(azarCol.nextDouble() * matrixCols);
-		
+
 		Node searched = searchNode(selectedRow, selectedCol);
-		
+
 		if(searched.getPosition() != 1 && searched.getPosition() != (matrixCols*matrixRows) && ladders != 0 && searched.getSnake() == ' ' && searched.getLadder() == 0){
 			int letter = (ladders);
 			searched.setLadder(letter);
@@ -234,15 +234,15 @@ public class SnakesAndLadders {
 			addSettingSnake(ladders);
 		}
 	}
-	
+
 	public Node searchNode(int row, int col) {
 		return searchNode(root, row, col);
 	}
-	
+
 	private Node searchNode(Node current, int row, int col) {
-		
+
 		Node searched = null;
-		
+
 		if(current == null || (current.getRow() == row && current.getCol() == col)) {
 			searched = current;
 		}
@@ -252,7 +252,7 @@ public class SnakesAndLadders {
 					searched = searchNode(current.getDown(), row, col);
 				}
 			}else if(current.getCol() < col) {
-				
+
 				if(current.getNext() != null) {
 					searched = searchNode(current.getNext(), row, col);
 				}
@@ -261,40 +261,70 @@ public class SnakesAndLadders {
 
 		return searched;
 	}
-	
+
 	public boolean splitString(String settings) {
-		
+
 		String setting[] = settings.split("");
 		boolean verify = true;
-		
+
 		try {
-			
+
 			int row = Integer.parseInt(setting[0]);
 			int col = Integer.parseInt(setting[2]);
 			int snakes = Integer.parseInt(setting[4]);
 			int ladders = Integer.parseInt(setting[6]);
 			int players = Integer.parseInt(setting[8]);
-			int index = 10;
 
-			setMatrixRows(row);
-			setMatrixCols(col);
-			
-			addSettingSnake(snakes);
-			addSettingLadders(ladders);
-			addSettingPlayers(setting, index);
-			
+			if(players <= 9) {
+				int index = 10;
+
+				if(players == ((setting.length + 1) - index)) {
+					addSettingPlayers(setting, index);
+
+				}else if(setting.length == 9) {
+					addSymbols(index-players);
+				}
+
+				setMatrixRows(row);
+				setMatrixCols(col);
+
+				addSettingSnake(snakes);
+				addSettingLadders(ladders);
+
+			}
 		}catch(NumberFormatException nfe){
-			System.out.println("Ingreso un valor INVALIDO");
-			verify = false;
+				verify = false;
 		}
 		return verify;
 	}
-	
+
 	public void addSettingPlayers(String[] settings, int index) {
-		
+
 		if(index == settings.length) {
 			Node first = getFirst(); 
 			first.setFirst(new Player(settings[index]));
+		}
+		else{
+			Node first = getFirst();
+			if(first.getFirst() != null) {
+				addSettingPlayers(first.getFirst(),settings, index + 1);
+			}
+		}
+	}
+	
+	private void addSettingPlayers(Player first,String[] settings,int index) {
+		if(index <= settings.length) {
+
+			first.setNext(new Player(settings[index]));
+			addSettingPlayers(first.getNext(),settings, index + 1);
+		}
+	}
+
+	public void addSymbols(int index) {
+		selectSymbols(index);
+
+		if(index == settings.length) {
+			
 		}
 		else{
 			Node first = getFirst();
@@ -303,15 +333,87 @@ public class SnakesAndLadders {
 			}
 		}
 	}
-	
-	private void addSettingPlayers(Player first,String[] settings,int index) {
-		if(index <= settings.length) {
+
+	private String selectSymbols(Player player,int index) {
+		String symbol = "";
+		boolean verify;
+
+		Random azarSymbols = new Random();
+		int selectedSymbol = (int)(azarSymbols.nextDouble() * 9);
+
+		if(index > 0) {
 			
-			first.setNext(new Player(settings[index]));
-			addSettingPlayers(first.getNext(),settings, index + 1);
+			switch(selectedSymbol) {
+			case 1:
+				symbol = "*";
+				verify = searchSymbols(symbol,9);
+				break;
+
+			case 2:
+				symbol = "!";
+				verify = searchSymbols(symbol,9);
+				break;
+
+			case 3:
+				symbol = "O";
+				verify = searchSymbols(symbol,9);
+				break;
+
+			case 4:
+				symbol = "X";
+				verify = searchSymbols(symbol,9);
+				break;
+
+			case 5:
+				symbol = "%";
+				verify = searchSymbols(symbol,9);
+				break;
+
+			case 6:
+				symbol = "$";
+				verify = searchSymbols(symbol,9);
+				break;
+
+			case 7:
+				symbol = "#";
+				verify = searchSymbols(symbol,9);
+				break;
+
+			case 8:
+				symbol = "+";
+				verify = searchSymbols(symbol,9);
+				break;
+
+			case 9:
+				symbol = "&";
+				verify = searchSymbols(symbol,9);
+				break;
+				
+			}
+			
+			Node first = getFirst();
+			
+			if(first.getFirst() == null){
+				first.setFirst(new Player(symbol));
+				selectSymbols(first.getFirst().getNext(),index - 1);
+				
+			}				
 		}
+		return symbol;
 	}
 	
+	private boolean searchSymbols(String symbol, int index) {
+		
+		boolean verify = true; 
+		Node first = getFirst();
+		
+		if(!first.getFirst().getSymbol().equals(symbol) && index > 0) {
+			searchSymbols(symbol, index - 1);
+			verify = false;
+		}
+		return verify;
+	}
+
 	private Node getFirst() {
 		return searchNode(matrixRows, 0);
 	}
---
+++ src/ui/Menu.java
@@ -0,0 +1,95 @@
+package ui;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import model.SnakesAndLadders;
+
+public class Menu {
+	private static BufferedReader br;
+	private SnakesAndLadders snakesAndLader;
+	
+	public Menu() throws IOException {
+		snakesAndLader = new SnakesAndLadders(0, 0);
+		System.out.println(snakesAndLader);
+		br = new BufferedReader(new InputStreamReader(System.in));
+		startApp();
+	}
+	
+	public void startApp() throws IOException{
+		System.out.println("==================================\n           BIENVENIDOS  \n==================================\n");
+		menu();
+	}
+
+	public void menu() throws IOException {
+		System.out.println("----------------------------------"
+				+ "\n      MENU DE OPCIONES\n----------------------------------\n1. Iniciar juego\n2. Puntuaciones\n"
+				+ "3. Salir de la aplicación\n4. Creditos\n----------------------------------");
+		selectOption();
+	}
+
+	public void selectOption() throws IOException {
+
+		System.out.println("\nIngrese un numero para elegir la opcion");
+		String number = br.readLine();
+
+		try {
+			int opt = Integer.parseInt(number);
+
+			if(opt != 3) {
+
+				switch(opt) {
+				case 1:
+					selectOpt1();
+					menu();
+					break;
+
+				case 2:
+					scores();
+					menu();
+					break;
+
+				case 4:
+					System.out.println("\n==================================\n            CREDITOS  \n==================================\n"
+							+ "  TRABAJO REALIZADO POR:\n\nLuis Miguel Ossa Arias\nSantiago Trochez Velasco\nAdicional: Juan Manuel Reyes\n");
+					menu();
+					break;
+
+				default:
+					System.out.println("Ingreso un numero NO VALIDO.");
+					selectOption();
+				}
+			}else {
+				System.out.println("\nGracias por usar la aplicacion :D.");
+			}
+
+		}catch(NumberFormatException nfe) {
+			System.out.println("Ingrese un numero como opcion");
+			selectOption();
+		}
+	}
+
+	public void selectOpt1() throws IOException {
+		System.out.println("\nIngrese los siguientes datos separandolos por espacios:\n"
+				+ "Numero de filas, numero de columnas, numero de serpientes, numero de escaleras, numero de jugadores, simbolos de cada jugador\n"
+				+ "--- Los simbolos de cada jugador van juntos, y si no agrega ninguno, se seleccionaran de forma aleatoria ---\n"
+				+ "\n* SIMBOLOS QUE PUEDE USAR: *, !, O, X, %, $, #, +, &\n");
+
+		String setting = br.readLine();
+		boolean verify = snakesAndLader.splitString(setting);
+
+		if(!verify) {
+			System.out.println("Ingreso un valor INVALIDO");
+			selectOpt1();
+		}
+	}
+
+	public void scores() {
+
+		String message = snakesAndLader.toStringScoreTable();
+
+		System.out.println("-----------------------------------"
+				+ "\n           PUNTUACIONES\n-----------------------------------\n"+ message +"\n-----------------------------------");
+	}
+}
