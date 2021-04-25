7b9806686077af1446b68223106069165fe29f4f
Luis Miguel <luismiguelossaarias05@gmail.com>
2021-04-24 20:41:42 -0500

    The class BestPlayers has been ajusted

--- src/model/BestPlayers.java
+++ src/model/BestPlayers.java
@@ -3,16 +3,29 @@ package model;
 public class BestPlayers {
 	
 	private String name;
+	private int row;
+	private int col;
+	private int snakes;
+	private int ladders;
+	private int players;
+	
 	private long score;
 	private String symbol;
+	private String otherPlayers;
 	
 	private BestPlayers next;
 	private BestPlayers previous;
 	
-	public BestPlayers(String name, long score, String symbol) {
+	public BestPlayers(String name, int row, int col, int snakes, int ladders, int players, long score, String symbol, String otherPlayers) {
 		this.name = name;
+		this.row = row;
+		this.col = col;
+		this.snakes = snakes;
+		this.ladders = ladders;
+		this.players = players;
 		this.score = score;
 		this.symbol = symbol;
+		this.otherPlayers = otherPlayers;
 	}
 
 	public String getName() {
@@ -53,6 +66,54 @@ public class BestPlayers {
 
 	public void setPrevious(BestPlayers previous) {
 		this.previous = previous;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+	public int getSnakes() {
+		return snakes;
+	}
+
+	public void setSnakes(int snakes) {
+		this.snakes = snakes;
+	}
+
+	public int getLadders() {
+		return ladders;
+	}
+
+	public void setLadders(int ladders) {
+		this.ladders = ladders;
+	}
+
+	public int getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(int players) {
+		this.players = players;
+	}
+
+	public String getOtherPlayers() {
+		return otherPlayers;
+	}
+
+	public void setOtherPlayers(String otherPlayers) {
+		this.otherPlayers = otherPlayers;
 	}
 
 	public String toString() {
--- src/model/SnakesAndLadders.java
+++ src/model/SnakesAndLadders.java
@@ -53,9 +53,9 @@ public class SnakesAndLadders {
 		}
 	}
 	
-	public void addPlayer(String name, long score, String symbol) {
+	public void addPlayer(String name, int row, int col, int snakes, int ladders, int players, long score, String symbol, String otherPlayers) {
 		
-		BestPlayers newPlayer = new BestPlayers(name,score,symbol);
+		BestPlayers newPlayer = new BestPlayers(name,row,col,snakes,ladders,players,score,symbol,otherPlayers);
 		
 		if(firstPlayer == null) {
 			firstPlayer = newPlayer;
